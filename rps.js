51ef1c4819b301e8b1b54e4ab54b827cfa4f3971
Rishabh <rishabh4883@gmail.com>
2022-07-21 21:18:16 +0530

    Finish up the project

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice;
@@ -27,3 +30,38 @@ function playRound(playerChoice, computerChoice) {
         // result = "You lose! " + computerChoice + " beats " + playerChoice;
     return winner;
 }
+
+function displayRoundResult(winner, playerChoice, computerChoice) {
+    switch(winner) {
+
+        case "tie": message = "It's a Tie!"; 
+            break;
+        case "player": message = "You win! " + playerChoice + " beats " + computerChoice; 
+            playerScore++; 
+            break;
+        case "computer": message = "You lose! " + computerChoice + " beats " + playerChoice;
+            computerScore++;
+            break;
+        default: message = "Invalid input";
+    }
+    console.log(message);    
+}
+
+function displayFinalResult() {
+    console.log(`Final Result: \nYou-${playerScore} \t Computer-${computerScore} `);
+    let winner = (playerScore > computerScore) ? "You're the winner!" : (playerScore < computerScore) ? "Computer wins!" : "It's a draw";
+    console.log(winner);
+}
+
+function game() {
+
+    for(let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Enter your choice: ");
+        let computerChoice = getComputerChoice();
+        winner = playRound(playerChoice, computerChoice);
+        displayRoundResult(winner, playerChoice, computerChoice);
+    }
+    displayFinalResult();
+}
+
+game();
